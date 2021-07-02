0a0cf5818507214655919f4866edc996834cab14
abraham.alvarez.bustos <abraham.alvarez.bustos@invenialabs.co.uk>
2021-07-02 14:05:37 +0100

    Remove double files and address test to example data

--- test/runtests.jl
+++ test/runtests.jl
@@ -14,7 +14,7 @@ const PSI = PowerSimulations
 
 @testset "MaximumGeneratorProfitTCRDD.jl" begin
     # Settings for testset
-    include("data/example_case4/buildup_case4.jl")
-    include("data/example_case4/c_sys_case4.jl")
+    include("../examples/data/example_case4/buildup_case4.jl")
+    include("../examples/data/example_case4/c_sys_case4.jl")
     include("max_gen_prof.jl")
 end
